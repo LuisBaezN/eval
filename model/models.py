f6579becb1ee38a30ba8b03045be6407839c5ebd
LuisBaezN <baez.201041081@gmail.com>
2023-07-21 18:14:15 -0400

    add: database

--- model/models.py
+++ model/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Productos(models.Model):
+    nombre = models.CharField(max_length=20)
+    descrip = models.TextField(max_length=200)
+    precio = models.FloatField()
+    cantidad = models.CharField(max_length=10)
+    perecedero = models.BooleanField()
